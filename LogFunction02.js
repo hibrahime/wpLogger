b304e7eeef383d56d37f7f145e84d426cfdff30b
halil.eryildiz <halil.eryildiz@inavitas.com>
2021-01-03 23:44:33 +0300

    test

---
+++ LogFunction02.js
@@ -0,0 +1,16 @@
+/*
+- message object msgText, toUser, startDate, endDate, atFirstOnline, isSent
+- usersToFollow[]
+- messagesToSend[]
+- sendMessage(messageObj) it can send message with message object 
+- ilk console.log usersToFollow[], messagesToSend[]
+- messagesToSend array should defined inside tofollowda
+- usersToFollow loop click each item  
+- check isOnline or not
+- is user have message , is startdate < t < enddate, issent false, isFirstOnline
+- send message func.
+*/
+
+function start() {
+
+}
