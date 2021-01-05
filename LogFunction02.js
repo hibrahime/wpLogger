303e1b38c89f605768f3c19d9f5ea51197cf14b8
halil.eryildiz <halilibrahim.eryildiz@hotmail.com>
2021-01-05 23:23:45 +0300

    base structure

--- LogFunction02.js
+++ LogFunction02.js
@@ -1,10 +1,5 @@
 /*
-- message object msgText, toUser, startDate, endDate, atFirstOnline, isSent
-- usersToFollow[]
-- messagesToSend[]
 - sendMessage(messageObj) it can send message with message object 
-- ilk console.log usersToFollow[], messagesToSend[]
-- messagesToSend array should defined inside tofollowda
 - usersToFollow loop click each item  
 - check isOnline or not
 - is user have message , is startdate < t < enddate, issent false, isFirstOnline
@@ -12,7 +7,21 @@
 - test
 - test5
 */
+const message = {
+    'text': 'test',
+    'to': 'person 1',
+    'startDate': '2020-01-05 07:40',
+    'endDate': '2020-01-05 10:40',
+    'sendAtFirstOnline': true,
+    'isSent': false
+};
+const peopleToFollow = [{'name': 'Person 2', 'isOnline': false}];
+const messagesToSend = [message];
 
 function start() {
-
+//addMessageReceiversToFollow
+    console.log('peopleToFollow',peopleToFollow);
+    console.log('messagesToSend',messagesToSend);
+    //loopInsidePeopleToFollow
+    //click each people
 }
