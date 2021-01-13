63550304729e97d77ef2ef1a018b331ef6266e05
halil.eryildiz <halilibrahim.eryildiz@hotmail.com>
2021-01-13 22:31:58 +0300

    test

--- LogFunction02.js
+++ LogFunction02.js
@@ -1,14 +1,14 @@
-// const document = {};
-
+const document = {};
+const window = {};
 const message = {
-  text: 'test',
-  to: 'person 1',
-  startDate: '2020-01-05 07:40',
-  endDate: '2020-01-05 10:40',
-  sendAtFirstOnline: true,
+  text: 'Dostum yeni çelıncımız nedir?',
+  to: 'Furkan Burak Bağcı',
+  startDate: '2021-01-13 21:30',
+  endDate: '2021-01-13 22:55',
+  sendAtFirstOnline: false,
   isSent: false,
 };
-const peopleToFollow = [{ name: 'Person 2', isOnline: false, onlineTimes: [] }];
+const peopleToFollow = [{ name: 'Furkan Burak Bağcı', isOnline: false, onlineTimes: [] }];
 const messagesToSend = [message];
 const isContinue = true;
 const counter = 0;
@@ -17,11 +17,11 @@ const changePersonSleep = 400;
 const waitValidStatusSleep = 3000;
 
 const htmlClasses = {
-  status: '_315-i',
+  status: '_3Id9P', // _3Id9P
   inPageUserName: '_19RFN',
   sendButton: 'button._3M-N-',
-  textBox: 'div._3u328',
-  contactsUserNames: '_19RFN',
+  textBox: 'div.DuUXI',// div.DuUXI
+  contactsUserNames: '_1hI5g', // _1hI5g
 };
 
 const statusTexts = {
@@ -36,16 +36,49 @@ const statusTexts = {
   },
 };
 
-function checkStatus() {
-  const statusText = document.getElementsByClassName(htmlClasses.status);
-  const statusTextString = statusText[0].innerHTML;
-  return statusTextString;
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function simulateMouseEvents(element, eventName) {
   const mouseEvent = document.createEvent('MouseEvents');
   mouseEvent.initEvent(eventName, true, true);
   element.dispatchEvent(mouseEvent);
+}
+
+function sendMessage({ text }) {
+  window.InputEvent = window.Event || window.InputEvent;
+
+  const event = new InputEvent('input', {
+    bubbles: true,
+  });
+  // document.querySelector(_1awRl).textContent = 'text'
+
+  const textbox = document.querySelector(htmlClasses.textBox);
+
+  textbox.textContent = text;
+  textbox.dispatchEvent(event);
+  console.log(`${text}`);
+  document.querySelector(htmlClasses.sendButton).click();
+}
+
+function isInsideDateRange(sd, ed) {
+  const [startDate, startTime] = sd.split(' ');
+  const [endDate, endTime] = ed.split(' ');
+  const [syear, smonth, sday] = startDate.split('-');
+  const [eyear, emonth, eday] = endDate.split('-');
+  const [shour, sminute] = startTime.split(':');
+  const [ehour, eminute] = endTime.split(':');
+  const dateNowEpoch = (new Date()).getTime();
+  const startDateEpoch = new Date(syear, smonth - 1, sday, shour, sminute).getTime();
+  const endDateEpoch = new Date(eyear, emonth - 1, eday, ehour, eminute).getTime();
+  return dateNowEpoch >= startDateEpoch && dateNowEpoch <= endDateEpoch;
+}
+
+function checkStatus() {
+  const statusText = document.getElementsByClassName(htmlClasses.status);
+  const statusTextString = statusText[0].innerHTML;
+  return statusTextString;
 }
 
 function clickPerson(person) {
@@ -62,31 +95,26 @@ function addMessageReceiversToFollow() {
   });
 }
 
-function isInsideDateRange(msg) {
-  console.log('message', msg);
-  return true;
-}
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+function help() {
+  console.log('peopleToFollow', peopleToFollow);
+  console.log('messagesToSend', messagesToSend);
 }
 
 async function start() {
   addMessageReceiversToFollow();
-
-  console.log('peopleToFollow', peopleToFollow);
-  console.log('messagesToSend', messagesToSend);
+  help();
 
   do {
     const person = peopleToFollow[counter % peopleToFollow.length];
     clickPerson(person);
     await sleep(checkOnlineSleep);
     const status = checkStatus();
-    if (Object.values(statusTexts.onlineTypes).includes(status)) {
-      const messages = messagesToSend.filter((m) => m.to === person
-      && isInsideDateRange(m)
+    person.isOnline = Object.values(statusTexts.onlineTypes).includes(status);
+    if (!Object.values(statusTexts.unknownTypes).includes(status)) {
+      const messages = messagesToSend.filter((m) => m.to === person.name
+      && isInsideDateRange(m.startDate, m.endDate)
       && !m.isSent
-      && ((message.sendAtFirstOnline && peopleToFollow.find((p) => p.name === message.to).isOnline)
+      && ((message.sendAtFirstOnline && person.isOnline)
         || !message.sendAtFirstOnline));
 
       if (messages && messages.length) {
@@ -96,7 +124,7 @@ async function start() {
           // sendMessage(msgObj)
         }
       }
-    } else if (Object.values(statusTexts.unknownTypes).includes(status)) {
+    } else {
       await sleep(waitValidStatusSleep);
     }
     await sleep(changePersonSleep);
